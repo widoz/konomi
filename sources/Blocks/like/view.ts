717ff8d88c1bcce11397c751cb07cb753f0b86f6
guido <dev@guidoscialfa.com>
2024-06-22 07:16:17 +0200

    Fetch

--- sources/Blocks/like/view.ts
+++ sources/Blocks/like/view.ts
@@ -1,6 +1,9 @@
 import { getContext, store } from '@wordpress/interactivity';
 
 type Context = {
+	entityId: number;
+	entityType: string;
+	userId: number;
 	isActive: boolean;
 };
 
@@ -10,6 +13,35 @@ store( 'konomi', {
 		toggleStatus: () => {
 			const context = getContext< Context >( 'konomi' );
 			context.isActive = ! context.isActive;
+		},
+	},
+
+	callbacks: {
+		updateUserPreferences: () => {
+			const {
+				entityId,
+				entityType,
+				isActive,
+				userId: id,
+			} = getContext< Context >( 'konomi' );
+
+			fetch( {
+				path: '/wp/v2/users',
+				method: 'POST',
+				data: {
+					id,
+					meta: {
+						_likes: { entityId, entityType },
+					},
+				},
+			} )
+				.then( ( response ) => {
+					console.log( response );
+				} )
+				.catch( ( error ) => {
+					console.error( error );
+					isActive = false;
+				} );
 		},
 	},
 } );
