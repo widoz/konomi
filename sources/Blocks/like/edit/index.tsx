7c5989deb68a9bfbc1dde0000ba04192fcc72a63
guido <dev@guidoscialfa.com>
2024-07-15 17:57:23 +0200

    Improper styling the block in Editor

--- sources/Blocks/like/edit/index.tsx
+++ sources/Blocks/like/edit/index.tsx
@@ -11,8 +11,10 @@ import { useBlockProps } from '@wordpress/block-editor';
 
 export function Edit(): JSX.Element {
 	return (
-		<button { ...useBlockProps() }>
-			<Icon icon="heart" />
-		</button>
+		<div className="konomi-like">
+			<button { ...useBlockProps() }>
+				<Icon icon="heart" />
+			</button>
+		</div>
 	);
 }
