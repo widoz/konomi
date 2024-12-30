f004907bb341dab2e55f98fbec7c06cd6b16cdaa
guido <dev@guidoscialfa.com>
2024-12-30 21:27:00 +0100

    Add Unit Test for Null Item
    Signed-off-by: guido <dev@guidoscialfa.com>

---
+++ tests/unit/php/User/NullItemTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Widoz\Wp\Konomi\User\NullItem;
+
+beforeEach(function () {
+    $this->nullItem = NullItem::new();
+});
+
+describe('Null Item', function(): void {
+    it('returns canned values', function (): void {
+        expect($this->nullItem->id())->toBe(0);
+        expect($this->nullItem->type())->toBe('');
+        expect($this->nullItem->isActive())->toBeFalse();
+        expect($this->nullItem->isValid())->toBeFalse();
+    });
+});
