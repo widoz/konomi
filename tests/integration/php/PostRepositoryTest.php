5c292cd3f88f3f2a49a65845f65ba05666335f43
Guido Scialfa <dev@guidoscialfa.com>
2025-05-08 23:03:31 +0200

    Update Post Repository Test

--- tests/integration/php/PostRepositoryTest.php
+++ tests/integration/php/PostRepositoryTest.php
@@ -40,31 +40,36 @@ describe('Post Repository', function (): void {
     it('find items from post repository', function (): void {
         $items = $this->repository->find(10, User\ItemGroup::REACTION);
 
-        expect($items)->toBeArray();
-        expect(count($items))->toBe(10);
+        expect($items)
+            ->toBeArray()
+            ->and(count($items))->toBe(10);
 
         foreach ($items as $userId => $item) {
-            expect($userId)->toBeInt();
-            expect($item instanceof User\Item)->toBe(true);
+            expect($userId)
+                ->toBeInt()
+                ->and($item instanceof User\Item)->toBeTrue();
         }
     });
 
     it('return empty collection if nothing found', function (): void {
         $items = $this->repository->find(1, User\ItemGroup::REACTION);
-        expect($items)->toBeArray();
-        expect(count($items))->toBe(0);
+        expect($items)
+            ->toBeArray()
+            ->and(count($items))->toBe(0);
     });
 
     it('return empty collection if the entity Id is zero', function (): void {
         $items = $this->repository->find(0, User\ItemGroup::REACTION);
-        expect($items)->toBeArray();
-        expect(count($items))->toBe(0);
+        expect($items)
+            ->toBeArray()
+            ->and(count($items))->toBe(0);
     });
 
     it('return empty collection if the entity Id is less than 0', function (): void {
         $items = $this->repository->find(rand(-100, -1), User\ItemGroup::REACTION);
-        expect($items)->toBeArray();
-        expect(count($items))->toBe(0);
+        expect($items)
+            ->toBeArray()
+            ->and(count($items))->toBe(0);
     });
 
     it('save items to post repository', function (): void {
@@ -76,12 +81,12 @@ describe('Post Repository', function (): void {
             $itemToStore->isActive()
         );
 
-        expect($result)->toBeTrue();
+        expect($result)->toBeTrue()
+            ->and($storedItem->id())->toEqual($itemToStore->id())
+            ->and($storedItem->type())->toEqual($itemToStore->type())
+            ->and($storedItem->isActive())->toEqual($itemToStore->isActive())
+            ->and($storedItem->isValid())->toBeTrue();
 
-        expect($storedItem->id())->toEqual($itemToStore->id());
-        expect($storedItem->type())->toEqual($itemToStore->type());
-        expect($storedItem->isActive())->toEqual($itemToStore->isActive());
-        expect($storedItem->isValid())->toBeTrue();
     });
 
     it('override existing item in post repository', function (): void {
@@ -91,8 +96,10 @@ describe('Post Repository', function (): void {
         $itemToStore = User\Item::new(1, 'type', false);
         $result = $this->repository->save($itemToStore, $this->currentUser);
 
-        expect($result)->toBeTrue();
-        expect($this->postMetaStorage[1]['_konomi_items.reaction'][$this->wpUser->ID])->toBeEmpty();
+        expect($result)
+            ->toBeTrue()
+            ->and($this->postMetaStorage[1]['_konomi_items.reaction'][$this->wpUser->ID])
+            ->toBeEmpty();
     });
 
     it('do not store invalid items', function (): void {
@@ -100,7 +107,8 @@ describe('Post Repository', function (): void {
         $this->repository->save($itemToStore, $this->currentUser);
         $result = $this->repository->save($itemToStore, $this->currentUser);
 
-        expect($result)->toBeFalse();
-        expect($this->postMetaStorage[-1]['_konomi_items.reaction'][$this->wpUser->ID])->toBeEmpty();
+        expect($result)
+            ->toBeFalse()
+            ->and($this->postMetaStorage[-1]['_konomi_items.reaction'][$this->wpUser->ID])->toBeEmpty();
     });
 });
