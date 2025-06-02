3b2bf7ed1b0cf353aeea755dd5c56df3d037385e
Guido Scialfa <dev@guidoscialfa.com>
2025-06-02 10:30:10 +0200

    Remove unnecessary InspectorControl Components

--- sources/Blocks/Bookmark/edit/inspector-controls.tsx
+++ sources/Blocks/Bookmark/edit/inspector-controls.tsx
@@ -1,5 +1,3 @@
-// @ts-expect-error
-import { InspectorControls as _InspectControls } from '@wordpress/block-editor';
 import type { JSX } from 'react';
 import React from 'react';
 import { ColorSettings } from './color-settings';
@@ -8,9 +6,5 @@ import type { BookmarkEdit } from './types';
 export function InspectorControls(
 	props: BookmarkEdit.EditProps
 ): JSX.Element {
-	return (
-		<_InspectControls group="styles">
-			<ColorSettings { ...props } />
-		</_InspectControls>
-	);
+	return <ColorSettings { ...props } />;
 }
--- sources/Blocks/Like/edit/inspector-controls.tsx
+++ sources/Blocks/Like/edit/inspector-controls.tsx
@@ -1,14 +1,8 @@
-// @ts-expect-error
-import { InspectorControls as _InspectControls } from '@wordpress/block-editor';
 import type { JSX } from 'react';
 import React from 'react';
 import { ColorSettings } from './color-settings';
 import type { LikeEdit } from './types';
 
 export function InspectorControls( props: LikeEdit.EditProps ): JSX.Element {
-	return (
-		<_InspectControls group="styles">
-			<ColorSettings { ...props } />
-		</_InspectControls>
-	);
+	return <ColorSettings { ...props } />;
 }
